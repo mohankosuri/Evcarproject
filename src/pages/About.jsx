ac59328cf29b48456a8b06134f11a4e63b27b171
mohankosuri <mkosuri@processqtech.com>
2024-06-06 17:13:10 +0530

    changes_in_about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,50 +1,9 @@
 import React from 'react'
+import AboutpageContent from '../components/AboutpageContent'
 
 const About = () => {
   return (
-    <div className='mx-[120px] my-[20px] bg-white p-10'>
-          <div className='font-bold  text-2xl'>About Ev-olution</div> 
-          <p className='mt-6'>
-          Welcome to Ev-olution, your premier destination for buying and selling top-quality vehicles. At Ev-olution, we believe that the journey to finding your perfect car should be as enjoyable and seamless as the drive itself.
-          
-          </p>
-          <div className='font-bold  text-2xl mt-10'>Who We Are</div> 
-          <p className='mt-4'>
-          Ev-olution is more than just a car dealership; we are a dedicated team of automotive enthusiasts with a passion for connecting people with their ideal vehicles. Our mission is to revolutionize the car buying experience by providing exceptional service, a diverse inventory, and a commitment to quality and transparency.
-
-          </p> 
-
-          <div className='font-bold  text-2xl mt-10'>What We Offer</div> 
-          <p className='mt-4'>
-          
-          <span className='font-bold'>Extensive Inventory:</span> Whether you're in the market for a sleek sedan, a rugged SUV, or an eco-friendly electric vehicle, Ev-olution has a wide range of options to suit every taste and budget. Our carefully curated selection includes both new and pre-owned vehicles from all major manufacturers.
-          
-          </p>
-          <p className='mt-4'>
-          
-          <span className='font-bold'>Quality Assurance:</span> Each vehicle in our inventory undergoes a thorough inspection to ensure it meets our high standards of quality and reliability. We provide detailed vehicle history reports and offer warranties to give you peace of mind with your purchase.
-          
-          </p>
-          <p className='mt-4'>
-          
-          <span className='font-bold'>Customer-Centric Service: </span> At Ev-olution, our customers are at the heart of everything we do. Our knowledgeable and friendly sales team is here to guide you through every step of the buying process, from initial consultation to final purchase. We pride ourselves on our transparency and honesty, ensuring you have all the information you need to make an informed decision.
-          
-          </p>
-          <p className='mt-4'>
-          
-          <span className='font-bold'>Convenient Financing Options:</span> We understand that buying a car is a significant investment. That's why we offer a variety of financing options tailored to your financial situation. Our finance experts work with leading banks and credit unions to secure the best rates and terms for you.
-          
-          </p>
-          <div className='font-bold  text-2xl mt-10'>Our Vision</div> 
-          <p className='mt-4'>
-          At Ev-olution, we aim to transform the car buying and selling process into a memorable and satisfying experience. We are dedicated to embracing innovation and leveraging the latest technology to provide our customers with a streamlined, efficient, and enjoyable experience.
-          </p>
-          <p className='mt-4'>
-          Thank you for choosing Ev-olution. We look forward to helping you find the perfect car to meet your needs and exceed your expectations.
-          
-          </p>
-    
-    </div>
+   <AboutpageContent/>
   )
 }
 
